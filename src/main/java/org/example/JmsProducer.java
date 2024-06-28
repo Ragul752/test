49d3b948306947fa6d70963d5c5ec8da92464c10
rdi7j8h <mdineshk019@gmail.com>
2024-06-28 19:51:09 +0200

    Done

---
+++ src/main/java/org/example/JmsProducer.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import jakarta.jms.*;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+
+import java.sql.SQLException;
+
+public class JmsProducer {
+    public void produce() throws JMSException {
+        String brokerURL = "tcp://localhost:61616";
+        String queueName = "TEST_QUEUE";
+
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(brokerURL);
+        Connection connection = connectionFactory.createConnection();
+        connection.start();
+        Session session = connection.createSession();
+
+        Destination destination = session.createQueue(queueName);
+
+        MessageProducer messageProducer = session.createProducer(destination);
+
+        String message = "This is a sample message";
+
+        TextMessage textMessage = session.createTextMessage(message);
+
+        messageProducer.send(textMessage);
+
+        session.close();
+        connection.close();
+    }
+    }
+
